41141e9d525c0111358d13b879a8551602929378
miaoyin <miaoyin@woda.com>
2023-11-20 10:16:23 +0800

    feat: lib web page

--- app/app.go
+++ app/app.go
@@ -18,25 +18,51 @@ type App struct {
 
 func NewApp(service *Service) *App {
 	var (
-		app      = &App{service: service, Bind: []any{service}}
-		appMenu  = menu.NewMenu()
-		helpMenu = appMenu.AddSubmenu("Help")
+		app     = &App{service: service, Bind: []any{service}}
+		appMenu = menu.NewMenu()
+		help    = appMenu.AddSubmenu("Help")
 	)
 
-	helpMenu.AddText("About", nil, app.About)
-	helpMenu.AddSeparator()
-	helpMenu.AddText("Quit", keys.CmdOrCtrl("q"), app.Quit)
+	help.AddText("About", nil, app.about)
+	help.AddSeparator()
+	help.AddText("Grephub", nil, app.grephub)
+	help.AddText("ripgrep (rg)", nil, app.rg)
+	help.AddText("ugrep (ug)", nil, app.ug)
+	help.AddText("The Silver Searcher (ag)", nil, app.ag)
+	help.AddText("ag win32", nil, app.agWin)
+	help.AddSeparator()
+	help.AddText("Quit", keys.CmdOrCtrl("q"), app.quit)
 	app.Menu = appMenu
 
 	return app
 }
 
-func (p *App) Quit(_ *menu.CallbackData) {
+func (p *App) quit(_ *menu.CallbackData) {
 	runtime.Quit(p.ctx)
 }
 
-func (p *App) About(_ *menu.CallbackData) {
+func (p *App) about(_ *menu.CallbackData) {
 	runtime.EventsEmit(p.ctx, "about", true)
+}
+
+func (p *App) grephub(_ *menu.CallbackData) {
+	runtime.BrowserOpenURL(p.ctx, "https://github.com/xuender/grephub")
+}
+
+func (p *App) rg(_ *menu.CallbackData) {
+	runtime.BrowserOpenURL(p.ctx, "https://github.com/BurntSushi/ripgrep")
+}
+
+func (p *App) ug(_ *menu.CallbackData) {
+	runtime.BrowserOpenURL(p.ctx, "https://github.com/Genivia/ugrep")
+}
+
+func (p *App) ag(_ *menu.CallbackData) {
+	runtime.BrowserOpenURL(p.ctx, "https://github.com/ggreer/the_silver_searcher")
+}
+
+func (p *App) agWin(_ *menu.CallbackData) {
+	runtime.BrowserOpenURL(p.ctx, "https://github.com/k-takata/the_silver_searcher-win32")
 }
 
 func (p *App) Startup(ctx context.Context) {
